5a2551c6afdb29e144e48b6f376164594d01b0bc
micknar <>
2021-05-12 00:24:38 +0300

    Добавлена возможность выбора даты при создании новой точки

--- src/presenter/point-new.js
+++ src/presenter/point-new.js
@@ -28,6 +28,7 @@ export default class PointNew {
 
     this._pointEditorComponent.setFormSubmitHandler(this._handleFormSubmit);
     this._pointEditorComponent.setDeleteClickHandler(this._handleDeleteClick);
+    this._pointEditorComponent.setDatepickers();
 
     render(this._pointsListContainer, this._pointEditorComponent, RenderPosition.AFTERBEGIN);
 
